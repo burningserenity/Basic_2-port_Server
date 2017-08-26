f3be635ab2e110fc7d69ad5226e2cf129c803f25
burningserenity <burningserenity@bitmai.la>
2017-08-26 11:55:27 -0400

    Added array for random sentences

--- 2-port.js
+++ 2-port.js
@@ -3,12 +3,19 @@ var http = require('http');
 var portOne = 7000;
 var portTwo = 7500;
 
+var niceThings = ["You are nice", "You are attractive", "You drive a nice car", "Everyone likes you", "You are doing a great job"];
+var meanThings = ["You are mean", "You are ugly", "You drive a shitty car", "Nobody likes you", "You are grossly incompetent"];
+var niceThing = "";
+var meanThing = "";
+
 function handleRequestOne(request, response) {
-    response.end("I love you\n");
+    niceThing = somethingGood();
+    response.end("I love you\n" + niceThing + "\n");
 }
 
 function handleRequestTwo(request, response) {
-    response.end("I hate you\n");
+    meanThing = somethingBad();
+    response.end("I hate you\n" + meanThing + "\n");
 }
 
 var serverOne = http.createServer(handleRequestOne);
@@ -22,6 +29,14 @@ serverTwo.listen(portTwo, function(){
     console.log("Server listening on: http://localhost:%s", portTwo);
 });
 
-//function somethingGood() {
+function somethingGood() {
+    var n = Math.floor(Math.random() * (niceThings.length - 0) + 0);
+    niceThing = niceThings[n];
+    return niceThing;
+}
 
-//}
+function somethingBad() {
+    var n = Math.floor(Math.random() * (meanThings.length - 0) + 0);
+    meanThing = meanThings[n];
+    return meanThing;
+}
